b6b28834e6f37f6309326c84b7f8c6b8fdc98668
christinae20492 <125934369+christinae20492@users.noreply.github.com>
2023-09-04 18:50:01 -0400

    final

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ const colors=["green","red","rgba(133,122,200)","#f15025"];
 const btn=document.getElementById('btn');
 const color=document.querySelector(".color");
 btn.addEventListener("click",function(){
-  //want to generate random number between 0-3
 const randomNumber=getRandomNumber();
 console.log(randomNumber);
 document.body.style.backgroundColor=colors[randomNumber];
 color.textContent=colors[randomNumber];
 })
 const getRandomNumber=()=>{
-  return Math.floor(Math.random()*3)};
+  return Math.floor(Math.random()*colors.length)};
